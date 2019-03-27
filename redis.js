1936409fd889313b9f20cc0dcc855f53e9bebced
icezeros <icezeros@163.com>
2019-03-27 15:19:16 +0800

    test

--- redis.js
+++ redis.js
@@ -1,11 +1,12 @@
 const Redis = require('ioredis');
 const config = require('./config');
-const redis = new Redis({
-    port: config.redis.port, // Redis port
-    host: config.redis.host, // Redis host
-    // family: 4, // 4 (IPv4) or 6 (IPv6)
-    password: config.redis.password,
-    db: config.redis.db,
-});
+const redis = new Redis(process.env.REDISCLOUD_URL);
+// const redis = new Redis({
+//     port: config.redis.port, // Redis port
+//     host: config.redis.host, // Redis host
+//     // family: 4, // 4 (IPv4) or 6 (IPv6)
+//     password: config.redis.password,
+//     db: config.redis.db,
+// });
 
 module.exports = redis;
